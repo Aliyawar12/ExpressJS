100b75f3bd1f5dfd33657adb424b54274ba492db
Muhammad Yawar Ali <141340403+Aliyawar12@users.noreply.github.com>
2024-07-25 18:03:22 +0500

    Update express-app.js

--- BackEnd/express-app.js
+++ BackEnd/express-app.js
@@ -9,7 +9,7 @@ const userRoutes = require("./Routes/userRoute");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || //Adjust the Port Number Here;
 
 
 //Connection to DataBase Call here
